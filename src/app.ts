b1d6f881c9d562c72a0c2a86cd9d4f83dfc42d0b
young <tlszn121@sookmyung.ac.kr>
2022-04-08 21:42:39 +0900

    add: logger

--- src/app.ts
+++ src/app.ts
@@ -8,16 +8,19 @@ import { connectDB } from './loaders/db';
 
 function startServer() {
     const app = express();
-    const logger = morgan('dev');
+    const logger = require('./api/middlewares/logger');
+    const morganFormat = process.env.NODE_ENV !== "production" ? "dev" : "combined";
 
     // db 연결
     // connectDB();
     
     app.use(cors());
-    app.use(logger);
+    app.use(morgan('HTTP/:http-version :method :url :status', { stream: logger.httpLogStream })); // NOTE: http request 로그 남기기
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
     app.use(cookieParser());
+
+
     // 라우팅
     app.use("/api", apiRouter);
 
@@ -27,17 +30,26 @@ function startServer() {
             success: false,
             message: "잘못된 경로입니다."
         });
+        //app log 남기기
+        const err = new Error(`잘못된 경로입니다.`);
+        logger.appLogger.log({
+            level: 'error',
+            message: err.message
+        })
     });
 
     app.listen(5050, () => {
         console.log(`
         ################################################
-        🛡️  Server listening on port 🛡️
+        🛡️  Server listening on port 5050🛡️
         ################################################
       `);
     })
-    .on("error", err => {
-        console.error(err);
+    .on("error", (err) => {
+        logger.appLogger().log({
+            level: 'error',
+            message: err.message
+        })
         process.exit(1);
     });
 }

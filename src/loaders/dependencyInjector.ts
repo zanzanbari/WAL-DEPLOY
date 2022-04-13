ccdd914a02a1d63cc64aa00b5ad092a514df123a
chanwoo <chocw0402@gmail.com>
2022-04-13 19:34:22 +0900

    [FEAT] di 시도 - 실패

---
+++ src/loaders/dependencyInjector.ts
@@ -0,0 +1,10 @@
+// import { User } from "@/models";
+// import Container from "typedi";
+// const logger = require("../api/middlewares/logger");
+
+// export default () => {
+//     Container.set([
+//         {id: "userRepository", value: new User()}
+//     ]),
+//     Container.set("logger", logger);
+// }

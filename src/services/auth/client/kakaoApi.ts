25a42dbfe1dc12d236a869cd60a05072a5194a49
chanwoo <chocw0402@gmail.com>
2022-04-13 19:36:15 +0900

    [FEAT] kakao 로그인 api

---
+++ src/services/auth/client/kakaoApi.ts
@@ -0,0 +1,35 @@
+import axios from "axios";
+import { Token, UserInfo } from "@/interface/dto/response/authResponse";
+const logger = require("../../../api/middlewares/logger");
+
+
+export async function KakaoAuthApi(
+    kakoAccessToken?: Token
+): Promise<UserInfo | undefined> { // 제발 오류처리 어케 할거야
+    try {
+
+        const apiUrl = "https://kapi.kakao.com/v2/user/me";
+        const reqConfig = {
+            headers: {
+                "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
+                "Authorization": `Bearer ${kakoAccessToken}`
+            }
+        };
+
+        const userData = await axios.post(apiUrl, {},reqConfig)
+            .then((resolve) => {
+                const nickname: string = resolve.data.properties["nickname"];
+                const email: string = resolve.data.kakao_account["email"];
+                return { nickname, email };
+            });
+
+        return userData;
+
+    } catch (error) {
+        logger.appLogger.log({
+            level: 'error',
+            message: error.message
+        })
+        throw new Error("❌ AXIOS_ERROR ❌");
+    }
+}

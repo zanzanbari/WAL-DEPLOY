703c3a015ab15700b4d9422411cb8d75ae528c36
chanwoo <chocw0402@gmail.com>
2022-04-14 12:12:34 +0900

    [ADD] token DTO

--- src/interface/dto/request/authRequest.ts
+++ src/interface/dto/request/authRequest.ts
@@ -1,5 +1,7 @@
 // 소셜 토큰과 fcm 토큰
 export interface TokenDto {
+    readonly accesstoken?: string,
+    readonly refreshtoken?: string,
     readonly socialtoken?: string,
     readonly fcmtoken?: string
 }

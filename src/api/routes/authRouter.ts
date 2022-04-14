943a10ff143e44959835cb1253a277ed9409ce94
chanwoo <chocw0402@gmail.com>
2022-04-14 12:10:24 +0900

    [ADD] auth 미들웨어, 토큰 재발급 라우터

--- src/api/routes/authRouter.ts
+++ src/api/routes/authRouter.ts
@@ -1,6 +1,8 @@
-import { celebrate, Joi } from "celebrate";
 import { Router } from "express";
+import { celebrate, Joi } from "celebrate";
+import authUtil from "../middlewares/auth";
 import { authController } from "../controllers/authController";
+// import validateUtil from "../middlewares/requestValidator";
 
 const router = Router();
 
@@ -10,7 +12,7 @@ router.post(
     // celebrate({
     //     params: Joi.object().keys({
     //         social: Joi.string().required()
-    //     }),
+    //     }).validate({}),
     //     query: Joi.object({
     //         socialToken: Joi.string().token().required()
     //         // fcmToken: Joi.string().token().required() 
@@ -18,7 +20,7 @@ router.post(
     // }),
     authController.socialLogin
 );
-
-router.get("/logout", authController.logout);
+router.get("/logout", authUtil.isAuth, authController.logout);
+router.post("/reissue/token", authController.reissueToken);
 
 export default router;

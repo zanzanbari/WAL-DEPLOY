ddc9eacc99a149deb57fe2404331988c66df6a13
chanwoo <chocw0402@gmail.com>
2022-04-14 12:02:07 +0900

    [FIX] 검증 모듈 수정

--- src/modules/validator.ts
+++ src/modules/validator.ts
@@ -1,4 +1,3 @@
-import jwt from "jsonwebtoken";
 const TOKEN_EXPIRED = -3;
 const TOKEN_INVALID = -2;
 
@@ -7,27 +6,6 @@ export const passwordValidator = (pwd: string) => {
     if (!password.test(pwd)) return false;        
     else return true;
 };
-
-
-export const verifyToken = (token: string) => {
-    let decoded: any;
-    try {
-        decoded = jwt.verify(token, process.env.JWT_SECRET);
-    } catch (error) {
-        if (error.message === "jwt expired") {
-            console.log("토큰이 만료되었습니다");
-            return TOKEN_EXPIRED;
-        } else if (error.message === "jwt invalid") {
-            console.log("토큰이 유효하지 않습니다");
-            return TOKEN_INVALID;
-        } else {
-            console.log("토큰 검증 오류")
-            return TOKEN_INVALID;
-        }
-    }
-    return decoded;
-}
-
 
 export function isTokenExpired (decoded: any) {
     return decoded === TOKEN_EXPIRED || decoded === TOKEN_INVALID

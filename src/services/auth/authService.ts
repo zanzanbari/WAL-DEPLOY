9b622832f6a44a82a5e0e7eca44d5656592ab7cf
chanwoo <chocw0402@gmail.com>
2022-04-14 12:00:03 +0900

    [FIX] authService 인터페이스 변경

--- src/services/auth/authService.ts
+++ src/services/auth/authService.ts
@@ -1,35 +1,8 @@
-import jwt from "jsonwebtoken";
 import { TokenDto } from "@/interface/dto/request/authRequest";
-import { Token, UserInfo } from "@/interface/dto/response/authResponse";
 
-abstract class AuthService {
-
-    protected async login(token: TokenDto): Promise<any> {};
-    protected async resign(): Promise<any> {};
-
-    protected async issueAccessToken(user: UserInfo | undefined): Promise<Token> {
-        const payload = {
-            id: user?.id,
-            nickname: user?.nickname,
-            email: user?.email,
-            social: user?.social
-        };
-        const accesstoken = jwt.sign(payload, process.env.JWT_SECRET, {
-            issuer: process.env.JWT_ISSUER,
-            expiresIn: process.env.JWT_AC_EXPIRES,
-        });
-
-        return accesstoken;
-    };
-
-    protected async issueRefreshToken(): Promise<Token> {
-        const refreshtoken = jwt.sign({}, process.env.JWT_SECRET, {
-            issuer: process.env.JWT_ISSUER,
-            expiresIn: process.env.JWT_RF_EXPIRES,
-        });
-
-        return refreshtoken;
-    };
+interface AuthService {
+    login(token: TokenDto): Promise<any>;
+    // protected async resign(): Promise<any> {};
 };
 
 export default AuthService;
--- src/services/auth/kakaoAuthService.ts
+++ src/services/auth/kakaoAuthService.ts
@@ -3,24 +3,25 @@ import AuthService from "./authService";
 import { KakaoAuthApi } from "./client/kakaoApi";
 import { TokenDto } from "@/interface/dto/request/authRequest";
 import { AuthResponse } from "@/interface/dto/response/authResponse";
+import { issueAccessToken, issueRefreshToken } from "@/modules/tokenHandller";
 
 @Service()
-class KakaoAuthService extends AuthService {
+class KakaoAuthService implements AuthService {
     // 주입해주고 싶다 
     constructor(
         private readonly userRepository: any,
         private readonly logger: any
     ) {
-        super();
     }
 
     public async login(request: TokenDto): Promise<AuthResponse | undefined> {
 
         try {
+            
             const userData = await KakaoAuthApi(request.socialtoken);
-            const refreshtoken = await this.issueRefreshToken();
+            const refreshtoken = await issueRefreshToken();
             const socialUser = await this.userRepository.findByEmailOrCreateSocialUser("kakao", userData, request, refreshtoken);
-            const accesstoken = await this.issueAccessToken(socialUser);
+            const accesstoken = await issueAccessToken(socialUser);
             
             const user: AuthResponse = {
                 nickname: socialUser.nickname,

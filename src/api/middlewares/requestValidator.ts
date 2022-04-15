7dae27dbff0dc08e855df4dec96394b5bca20489
chanwoo <chocw0402@gmail.com>
2022-04-15 18:53:05 +0900

    [FEAT] request validate 핸들링

---
+++ src/api/middlewares/requestValidator.ts
@@ -0,0 +1,58 @@
+import Joi from "joi";
+import { NextFunction, Request, Response } from "express";
+import sc from "@/constant/resultCode";
+import rm from "@/constant/resultMessage";
+import { ErrorResponse } from "@/modules/apiResponse";
+import { SocialType, TokenDto } from "@/interface/dto/request/authRequest";
+const logger = require("../../api/middlewares/logger");
+
+
+const toLogin = async (
+  req: Request, 
+  res: Response, 
+  next: NextFunction
+) => {
+  
+  const loginParamSchema = Joi.object().keys({
+    social: Joi.string().required().valid("apple", "kakao"),
+  });
+  const loginQuerySchema = Joi.object().keys({
+    socialtoken: Joi.string().token().required(),
+    // fcmtoken: Joi.string().token().required()
+  });
+  
+  try {
+    // validate 쓰면 error 속성 존재, validateAsync 쓰면 없고 catch error 해줘야함 
+    // -> 비동기 최대로 활용 못하는거 같아서 좀 아쉬움
+    const paramsError = await loginParamSchema
+      .validateAsync(req.params as SocialType)
+      .catch(error => {
+        return error;
+      });
+
+    const queryError = await loginQuerySchema
+      .validateAsync(req.query as TokenDto)
+      .catch(error => {
+        return error;
+      });
+      
+    if ( paramsError.details || queryError.details) { // ( error 에만 존재하는 detail )
+      return ErrorResponse(res, sc.BAD_REQUEST, rm.WRONG_PARAMS_OR_NULL);
+    }
+
+    next();
+        
+  } catch (error) {
+    console.error(`[VALIDATE ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`);
+    logger.appLogger.log({ level: "error", message: error.message}); 
+    ErrorResponse(res, sc.BAD_REQUEST, rm.WRONG_PARAMS_OR_NULL);  
+  }
+};
+
+
+
+const validateUtil = {
+  toLogin,
+}
+
+export default validateUtil;
--- src/api/routes/authRouter.ts
+++ src/api/routes/authRouter.ts
@@ -1,26 +1,21 @@
 import { Router } from "express";
-import { celebrate, Joi } from "celebrate";
 import authUtil from "../middlewares/auth";
 import { authController } from "../controllers/authController";
-// import validateUtil from "../middlewares/requestValidator";
+import validateUtil from "../middlewares/requestValidator";
 
 const router = Router();
 
 // router.get()
 router.post(
-    "/:social/login",
-    // celebrate({
-    //     params: Joi.object().keys({
-    //         social: Joi.string().required()
-    //     }).validate({}),
-    //     query: Joi.object({
-    //         socialToken: Joi.string().token().required()
-    //         // fcmToken: Joi.string().token().required() 
-    //     })
-    // }),
+    "/:social/login", 
+    validateUtil.toLogin,
     authController.socialLogin
 );
-router.get("/logout", authUtil.isAuth, authController.logout);
+router.get(
+    "/logout", 
+    authUtil.isAuth, 
+    authController.logout
+);
 router.post("/reissue/token", authController.reissueToken);
 
 export default router;

2d0dfde5dc3483ef96da5182ab3c948d73ecaccc
chanwoo <chocw0402@gmail.com>
2022-04-15 18:53:50 +0900

    [CHORE]

--- src/constant/resultMessage.ts
+++ src/constant/resultMessage.ts
@@ -1,6 +1,7 @@
 const ResultMessage = {
   NULL_VALUE: "필요한 값이 없습니다",
-  OUT_OF_VALUE: "파라미터 값이 잘못되었습니다",
+  WRONG_PARAMS: "파라미터 값이 잘못되었습니다",
+  WRONG_PARAMS_OR_NULL: "파라미터 값이 없거나 잘못되었습니다",
   DB_ERROR: "디비 에러",
   
   // 회원가입

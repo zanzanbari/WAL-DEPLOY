bc5520548a9337081ed3403787ba60140d644569
young <tlszn121@sookmyung.ac.kr>
2022-04-25 04:12:25 +0900

    ec2 deploy test router

--- src/api/routes/index.ts
+++ src/api/routes/index.ts
@@ -3,5 +3,7 @@ import authRouter from "./authRouter";
 const router = Router();
 
 router.use("/auth", authRouter);
-
+router.use("/test", (req, res)=> {
+    res.status(400).json("배포 test");
+})
 export default router;

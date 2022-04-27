878ab131c76a98445103f26897e80a7e5e1ccf15
chanwoo <chocw0402@gmail.com>
2022-04-28 01:24:39 +0900

    [ADD] user dto

--- src/interface/dto/request/userRequest.ts
+++ src/interface/dto/request/userRequest.ts
@@ -2,6 +2,18 @@
 export interface UserSettingDto {
     readonly nickname?: string,
     readonly dtype?: number[],
-    readonly time?: number[]
+    readonly time?: string[]
 }
 
+export interface UserSetTime {
+    // [ time: string ]: boolean
+    morning?: boolean,
+    afternoon?: boolean,
+    night?: boolean,
+}
+
+export interface UserSetCategory {
+    user_id?: number,
+    category_id?: number,
+    next_item_id?: number,
+}

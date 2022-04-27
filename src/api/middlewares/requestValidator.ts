0fd199dd074aed8df856761da218af2b6a42fe10
chanwoo <chocw0402@gmail.com>
2022-04-28 01:26:47 +0900

    [ADD] 초기세팅 validate

--- src/api/middlewares/requestValidator.ts
+++ src/api/middlewares/requestValidator.ts
@@ -4,10 +4,11 @@ import sc from "@/constant/resultCode";
 import rm from "@/constant/resultMessage";
 import { ErrorResponse } from "@/modules/apiResponse";
 import { SocialType, TokenDto } from "@/interface/dto/request/authRequest";
+import { UserSettingDto } from "@/interface/dto/request/userRequest";
 const logger = require("../../api/middlewares/logger");
 
 // fcmtoken optional 로 한거 개맘에 안드는데,,, isLogin 따로 빼면 코드 중복 개쩔거같고,,, 고민
-const loginCheck = async (
+const loginRequestCheck = async (
   req: Request, 
   res: Response, 
   next: NextFunction
@@ -32,9 +33,7 @@ const loginCheck = async (
 
     const queryError = await loginQuerySchema
       .validateAsync(req.query as TokenDto)
-      .catch(error => {
-        return error;
-      });
+      .catch(error => { return error; });
       
     if ( paramsError.details || queryError.details) { // ( error 에만 존재하는 detail )
       return ErrorResponse(res, sc.BAD_REQUEST, rm.WRONG_PARAMS_OR_NULL);
@@ -45,14 +44,48 @@ const loginCheck = async (
   } catch (error) {
     console.error(`[VALIDATE ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`);
     logger.appLogger.log({ level: "error", message: error.message}); 
-    ErrorResponse(res, sc.BAD_REQUEST, rm.WRONG_PARAMS_OR_NULL);  
+    ErrorResponse(res, sc.INTERNAL_SERVER_ERROR, rm.INTERNAL_SERVER_ERROR);  
   }
 };
 
 
+const initRequestCheck = async (
+  req: Request, 
+  res: Response, 
+  next: NextFunction
+) => {
+
+  const setInfoSchema = Joi.object().keys({
+    nickname: Joi.string().required().max(10),
+    dtype: Joi.array().items(Joi.string()).required(),
+    time: Joi.array().items(Joi.string()).required()
+  });
+
+  try {
+
+    const bodyError = await setInfoSchema
+      .validateAsync(req.body as UserSettingDto)
+      .catch(error => { return error; });
+
+    if (bodyError.details) {
+      return ErrorResponse(res, sc.BAD_REQUEST, rm.WRONG_PARAMS_OR_NULL);
+    }
+
+    next();
+
+  } catch (error) {
+    console.error(`[VALIDATE ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`);
+    logger.appLogger.log({ level: "error", message: error.message}); 
+    ErrorResponse(res, sc.INTERNAL_SERVER_ERROR, rm.INTERNAL_SERVER_ERROR);  
+  }
+
+}
+
+
 
 const validateUtil = {
-  loginCheck,
+  loginRequestCheck,
+  initRequestCheck
 }
 
 export default validateUtil;

3eec1aebaa4940ac2b8bc4d462733511420425ca
chanwoo <chocw0402@gmail.com>
2022-04-28 03:46:33 +0900

    [ADD] user info response

--- src/interface/dto/response/userResponse.ts
+++ src/interface/dto/response/userResponse.ts
@@ -1,3 +1,11 @@
+import { UserSetTime } from "../request/userRequest";
+import { UserInfo } from "./authResponse";
+
 export interface UserSettingResponse {
   nickname?: string
+}
+
+export interface UserInfoResponse extends UserInfo {
+  readonly times?: UserSetTime,
+  readonly categories?: number[]
 }

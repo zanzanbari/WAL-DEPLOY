3d0174b07a89d7a3c83f7cac4eaceaaf953ce2e3
chanwoo <chocw0402@gmail.com>
2022-04-29 02:15:00 +0900

    [ADD] 알람시간 수정 validate

--- src/api/middlewares/requestValidator.ts
+++ src/api/middlewares/requestValidator.ts
@@ -4,7 +4,7 @@ import sc from "@/constant/resultCode";
 import rm from "@/constant/resultMessage";
 import { ErrorResponse } from "@/modules/apiResponse";
 import { SocialType, TokenDto } from "@/interface/dto/request/authRequest";
-import { UserSettingDto } from "@/interface/dto/request/userRequest";
+import { UserSetTime, UserSettingDto } from "@/interface/dto/request/userRequest";
 const logger = require("../../api/middlewares/logger");
 
 // fcmtoken optional 로 한거 개맘에 안드는데,,, isLogin 따로 빼면 코드 중복 개쩔거같고,,, 고민
@@ -27,13 +27,11 @@ const loginRequestCheck = async (
     // -> 비동기 최대로 활용 못하는거 같아서 좀 아쉬움
     const paramsError = await loginParamSchema
       .validateAsync(req.params as SocialType)
-      .catch(error => {
-        return error;
-      });
+      .catch(err => { return err; });
 
     const queryError = await loginQuerySchema
       .validateAsync(req.query as TokenDto)
-      .catch(error => { return error; });
+      .catch(err => { return err; });
       
     if ( paramsError.details || queryError.details) { // ( error 에만 존재하는 detail )
       return ErrorResponse(res, sc.BAD_REQUEST, rm.WRONG_PARAMS_OR_NULL);
@@ -65,7 +63,7 @@ const initRequestCheck = async (
 
     const bodyError = await setInfoSchema
       .validateAsync(req.body as UserSettingDto)
-      .catch(error => { return error; });
+      .catch(err => { return err; });
 
     if (bodyError.details) {
       return ErrorResponse(res, sc.BAD_REQUEST, rm.WRONG_PARAMS_OR_NULL);
@@ -83,9 +81,44 @@ const initRequestCheck = async (
 
 
 
+const timeRequestCheck = async (
+  req: Request, 
+  res: Response, 
+  next: NextFunction
+) => {
+
+  const timeSchema = Joi.object().keys({
+    morning: Joi.boolean().required(),
+    afternoon: Joi.boolean().required(),
+    night: Joi.boolean().required(),
+  });
+
+  try {
+
+    const bodyError = await timeSchema
+      .validateAsync(req.body as UserSetTime)
+      .catch(err => { return err; });
+
+    if (bodyError.details) {
+      return ErrorResponse(res, sc.BAD_REQUEST, rm.WRONG_PARAMS_OR_NULL);
+    }
+
+    next();
+
+  } catch (error) {
+    console.error(`[VALIDATE ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`);
+    logger.appLogger.log({ level: "error", message: error.message}); 
+    ErrorResponse(res, sc.INTERNAL_SERVER_ERROR, rm.INTERNAL_SERVER_ERROR); 
+  }
+
+}
+
+
+
 const validateUtil = {
   loginRequestCheck,
-  initRequestCheck
+  initRequestCheck,
+  timeRequestCheck
 }
 
 export default validateUtil;

76391261d8f13631f1c520a2cb4b338dd67effef
chanwoo <chocw0402@gmail.com>
2022-05-09 02:09:07 +0900

    [CHORE] 줄 간격 변경

--- src/modules/timeHandler.ts
+++ src/modules/timeHandler.ts
@@ -1,21 +1,45 @@
-import dayjs from "dayjs";
-
-
 const getMorning = () => {
   const setTime = new Date();
-  const morning = new Date(Date.UTC(setTime.getFullYear(),setTime.getMonth(),setTime.getDate(),8,0,0,0));
+  const morning = new Date(
+    Date.UTC(
+      setTime.getFullYear(),
+      setTime.getMonth(),
+      setTime.getDate(),
+      8,
+      0,
+      0,
+      0
+    ));
   return morning;
 }
 
 const getAfternoon = () => {
   const setTime = new Date();
-  const afternoon = new Date(Date.UTC(setTime.getFullYear(),setTime.getMonth(),setTime.getDate(),14,0,0,0));
+  const afternoon = new Date(
+    Date.UTC(
+      setTime.getFullYear(),
+      setTime.getMonth(),
+      setTime.getDate(),
+      14,
+      0,
+      0,
+      0
+    ));
   return afternoon;
 }
 
 const getNight = () => {
   const setTime = new Date();
-  const night = new Date(Date.UTC(setTime.getFullYear(),setTime.getMonth(),setTime.getDate(),20,0,0,0));
+  const night = new Date(
+    Date.UTC(
+      setTime.getFullYear(),
+      setTime.getMonth(),
+      setTime.getDate(),
+      20,
+      0,
+      0,
+      0
+    ));
   return night;
 }
 

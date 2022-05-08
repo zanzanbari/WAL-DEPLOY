5cfb9891d2a0afe1187ef849246173b24d97df8a
chanwoo <chocw0402@gmail.com>
2022-05-08 18:47:01 +0900

    [ADD] 시간 핸들

---
+++ src/modules/timeHandler.ts
@@ -0,0 +1,28 @@
+import dayjs from "dayjs";
+
+
+const getMorning = () => {
+  const setTime = new Date();
+  const morning = new Date(Date.UTC(setTime.getFullYear(),setTime.getMonth(),setTime.getDate(),8,0,0,0));
+  return morning;
+}
+
+const getAfternoon = () => {
+  const setTime = new Date();
+  const afternoon = new Date(Date.UTC(setTime.getFullYear(),setTime.getMonth(),setTime.getDate(),14,0,0,0));
+  return afternoon;
+}
+
+const getNight = () => {
+  const setTime = new Date();
+  const night = new Date(Date.UTC(setTime.getFullYear(),setTime.getMonth(),setTime.getDate(),20,0,0,0));
+  return night;
+}
+
+const timeHandler = {
+  getMorning,
+  getAfternoon,
+  getNight
+};
+
+export default timeHandler;

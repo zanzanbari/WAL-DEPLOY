38a265f47b87d4624b8ab5cf60524240d29161a7
young <tlszn121@sookmyung.ac.kr>
2022-05-08 18:59:10 +0900

    pem, cer deploy ignore

--- dist/modules/timeHandler.js
+++ dist/modules/timeHandler.js
@@ -1,15 +1,5 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-const dayjs_1 = __importDefault(require("dayjs"));
-const getCurrentTime = () => {
-    const today = new Date();
-    const currentTime = `${today.getHours()}:${today.getMinutes()}:${today.getSeconds()}`;
-    const formatTime = (0, dayjs_1.default)(currentTime).format("HH:mm:ss");
-    return formatTime;
-};
 const getMorning = () => {
     const setTime = new Date();
     const morning = new Date(Date.UTC(setTime.getFullYear(), setTime.getMonth(), setTime.getDate(), 8, 0, 0, 0));
@@ -26,7 +16,6 @@ const getNight = () => {
     return night;
 };
 const timeHandler = {
-    getCurrentTime,
     getMorning,
     getAfternoon,
     getNight

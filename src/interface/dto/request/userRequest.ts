d4620caca468fd14dc4905fa577f57f8c27b77af
chanwoo <chocw0402@gmail.com>
2022-05-09 02:08:01 +0900

    [ADD]TodayWal Dto 추가

--- src/interface/dto/request/userRequest.ts
+++ src/interface/dto/request/userRequest.ts
@@ -3,20 +3,20 @@ export interface UserSettingDto {
     readonly nickname?: string,
     readonly dtype?: ISetCategory,
     readonly time?: ISetTime,
-}
+};
 
 export interface ISetTime {
     // [ time: string ]: boolean
     morning?: boolean,
     afternoon?: boolean,
     night?: boolean,
-}
+};
 
 export interface ISetUserCategory {
     user_id?: number,
     category_id?: number,
     next_item_id?: number,
-}
+};
 
 export type ResetTimeDto = ISetTime[];
 export type ResetCategoryDto = ISetCategory[];
@@ -27,4 +27,12 @@ export interface ISetCategory {
     compliment: boolean,
     condolence: boolean,
     scolding: boolean
-}
+};
+
+
+export interface ISetTodayWal {
+    user_id: number;
+    category_id: number;
+    item_id: number;
+    time: Date;
+};

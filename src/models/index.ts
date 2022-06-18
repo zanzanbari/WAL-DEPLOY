5a35786cc13e02db8efb41ec58fbe17d177d3599
chanwoo <chocw0402@gmail.com>
2022-06-19 00:03:42 +0900

    [CHORE] 쿼리문 확인

--- src/models/index.ts
+++ src/models/index.ts
@@ -14,7 +14,7 @@ const sequelize = new Sequelize({
     username: config.database.development.username,
     password: config.database.development.password,
     dialect: "postgres",
-    logging: false,
+    logging: true,
     timezone: "+09:00",
     dialectOptions: {
         charset: 'utf8mb4',

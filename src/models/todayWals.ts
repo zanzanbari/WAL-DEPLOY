881cd238ec3c1ea3ed73fd46c23bb979c74f55a9
chanwoo <chocw0402@gmail.com>
2022-06-21 04:02:01 +0900

    [REFACTOR] models 리팩토링

--- src/models/todayWals.ts
+++ src/models/todayWals.ts
@@ -1,86 +1,147 @@
 import { 
-    AutoIncrement, 
-    BelongsTo, 
-    Column, 
-    DataType, 
-    AllowNull,
-    ForeignKey, 
-    Model, 
-    PrimaryKey, 
-    Table, 
-    Unique, 
-    Default} from "sequelize-typescript"
+  AutoIncrement, 
+  BelongsTo, 
+  Column, 
+  DataType, 
+  AllowNull,
+  ForeignKey, 
+  Model, 
+  PrimaryKey, 
+  Table, 
+  Unique, 
+  Default } from "sequelize-typescript"
 import User from "./users";
 import Item from "./items";
 import Reservation from "./reservations";
 import { ISetTodayWal } from "../dto/request/userRequest";
 
 @Table({ // 테이블 설정
-    modelName: "TodayWal",
-    tableName: "todayWals",
-    freezeTableName: true,
-    underscored: false,
-    paranoid: false,
-    timestamps: false,
-    charset: "utf8", 
-    collate: "utf8_general_ci", 
+  modelName: "TodayWal",
+  tableName: "todayWals",
+  freezeTableName: true,
+  underscored: false,
+  paranoid: false,
+  timestamps: false,
+  charset: "utf8", 
+  collate: "utf8_general_ci", 
 })
 
 export default class TodayWal extends Model { 
-    @PrimaryKey
-    @AutoIncrement
-    @Unique
-    @Column
-    id!: number;
+  @PrimaryKey
+  @AutoIncrement
+  @Unique
+  @Column
+  id!: number;
 
-    @ForeignKey(() => User)
-    @Column(DataType.INTEGER)
-    user_id!: number;
+  @ForeignKey(() => User)
+  @Column(DataType.INTEGER)
+  user_id!: number;
 
-    @ForeignKey(() => Item)
-    @Column(DataType.INTEGER)
-    item_id!: number;
+  @ForeignKey(() => Item)
+  @Column(DataType.INTEGER)
+  item_id!: number;
 
-    @ForeignKey(() => Reservation)
-    @Column(DataType.INTEGER)
-    reservation_id!: number;
+  @ForeignKey(() => Reservation)
+  @Column(DataType.INTEGER)
+  reservation_id!: number;
 
-    @AllowNull(false)
-    @Default(false)
-    @Column(DataType.BOOLEAN)
-    public userDefined!: Boolean;
+  @AllowNull(false)
+  @Default(false)
+  @Column(DataType.BOOLEAN)
+  public userDefined!: Boolean;
 
-    @AllowNull(false)
-    @Column(DataType.DATE)
-    public time!: Date;
+  @AllowNull(false)
+  @Column(DataType.DATE)
+  public time!: Date;
 
-    @BelongsTo(() => User)
-    user!: User;
+  @BelongsTo(() => User)
+  user!: User;
 
-    @BelongsTo(() => Item)
-    item!: Item;
+  @BelongsTo(() => Item)
+  item!: Item;
 
-    @BelongsTo(() => Reservation)
-    reservation!: Reservation;
+  @BelongsTo(() => Reservation)
+  reservation!: Reservation;
 
-    static async setTodayWal(data: ISetTodayWal) {
-        await this.create({ ...data });
-    }
+  static async setTodayWal(data: ISetTodayWal) {
+    await this.create({ ...data });
+  };
 
-    static async getTodayWalsByUserId(id: number): Promise<TodayWal[]> {
-        const todayWals = await this.findAll({
-            where: {
-                user_id: id,
-            },
-            order: [
-                ["time", "ASC"]
-            ]
-        })
-        return todayWals;
-    }
+  static async getTodayWalsByUserId(id: number): Promise<TodayWal[]> {
+    const todayWals = await this.findAll({
+      where: {
+        user_id: id,
+      },
+      order: [
+        ["time", "ASC"]
+      ]
+    });
+    return todayWals;
+  };
+
+  static async getTodayReservation(user_id: number, reservation_id: number) {
+    return await this.findOne({
+      where: { 
+        user_id, 
+        reservation_id,
+        userDefined: true
+      }
+    });
+  };
     
-    static async deleteTodayWal(user_id: number, time?: Date) {
-        await this.destroy({ where: { user_id, time } });
+  static async deleteTodayWal(user_id: number, time?: Date) {
+    await this.destroy({ where: { user_id, time } });
+  };
+
+  static async deleteAll() {
+    await this.destroy({
+      where: {},
+      truncate: true
+    });
+  };
+
+
+  static async getFcmByUserId(user_id: number, time?: Date): Promise<{
+    fcmtoken: string;
+    itemId: number;
+  } | {
+    fcmtoken: string;
+    reservationId: number;
+  }> {
+    
+    if (time) {
+
+      const wal = await this.findOne({
+        where: { user_id, time },
+        include: [
+          { model: User, attributes: ["fcmtoken"] }
+        ]
+      });
+
+      return {
+        fcmtoken: wal?.getDataValue("user").getDataValue("fcmtoken") as string,
+        itemId: wal?.getDataValue("item_id") as number
+      };
+
+    } else {
+
+      const wal = await this.findOne({
+        where: {
+          user_id,
+          userDefined: true
+        },
+        include: [
+          { model: User, attributes: ["fcmtoken"] }
+        ]
+      });
+
+      return { 
+        fcmtoken: wal?.getDataValue("user").getDataValue("fcmtoken") as string,
+        reservationId: wal?.getDataValue("reservation_id") as number
+      };
+      
     }
+
+  };
 
 }
--- src/models/users.ts
+++ src/models/users.ts
@@ -1,198 +1,201 @@
 import { 
-    AllowNull, 
-    AutoIncrement, 
-    Column, 
-    DataType, 
-    HasOne,
-    HasMany, 
-    IsEmail, 
-    Model, 
-    PrimaryKey, 
-    Table, 
-    Unique } from "sequelize-typescript"
+  AllowNull, 
+  AutoIncrement, 
+  Column, 
+  DataType, 
+  HasOne,
+  HasMany, 
+  Model, 
+  PrimaryKey, 
+  Table, 
+  Unique } from "sequelize-typescript"
 import Time from "./times";
 import Reservation from "./reservations";
 import UserCategory from "./userCategories";
 import rm from "../constant/resultMessage";
 import { TokenDto } from "../dto/request/authRequest";
 import { Token, UserInfo } from "../dto/response/authResponse";
+import { Op } from "sequelize";
 
 @Table({
-    modelName: "User",
-    tableName: "users",
-    freezeTableName: true,
-    underscored: false,
-    paranoid: false,
-    timestamps: true,
-    charset: "utf8", // 한국어 설정
-    collate: "utf8_general_ci", // 한국어 설정
+  modelName: "User",
+  tableName: "users",
+  freezeTableName: true,
+  underscored: false,
+  paranoid: false,
+  timestamps: true,
+  charset: "utf8", // 한국어 설정
+  collate: "utf8_general_ci", // 한국어 설정
 })
 
 export default class User extends Model {
-    @PrimaryKey
-    @AutoIncrement
-    @Unique
-    @Column(DataType.INTEGER)
-    public readonly id!: number;
+  @PrimaryKey
+  @AutoIncrement
+  @Unique
+  @Column(DataType.INTEGER)
+  public readonly id!: number;
 
-    @AllowNull(false)
-    @Column(DataType.STRING(15))
-    public social!: string;
-
-
-    @AllowNull(false)
-    @Unique
-    @Column(DataType.STRING(50))
-    public email!: string;
+  @AllowNull(false)
+  @Column(DataType.STRING(15))
+  public social!: string;
 
 
-    @AllowNull(true)
-    @Column(DataType.STRING(20))
-    public nickname?: string | null;
+  @AllowNull(false)
+  @Unique
+  @Column(DataType.STRING(50))
+  public email!: string;
 
 
-    @AllowNull(true)
-    @Column(DataType.STRING(100))
-    public password?: string;
+  @AllowNull(true)
+  @Column(DataType.STRING(20))
+  public nickname?: string | null;
 
 
-    @AllowNull(true)
-    @Unique
-    @Column(DataType.TEXT)
-    public refreshtoken?: string;   
+  @AllowNull(true)
+  @Column(DataType.STRING(100))
+  public password?: string;
 
 
-    @AllowNull(true)
-    @Unique
-    @Column(DataType.TEXT)
-    public fcmtoken?: string;   
-
-    @HasMany(() => UserCategory)
-    userCategories!: UserCategory[];
-
-    @HasOne(() => Time)
-    time!: Time
-
-    @HasMany(() => Reservation)
-    reservations!: Reservation[];
+  @AllowNull(true)
+  @Unique
+  @Column(DataType.TEXT)
+  public refreshtoken?: string;   
 
 
-    /*
-     * custom method 
-     */
+  @AllowNull(true)
+  @Unique
+  @Column(DataType.TEXT)
+  public fcmtoken?: string;   
+
+  @HasMany(() => UserCategory)
+  userCategories!: UserCategory[];
+
+  @HasOne(() => Time)
+  time!: Time
+
+  @HasMany(() => Reservation)
+  reservations!: Reservation[];
+
+
+  /*
+   * custom method 
+   */
     
-    static async findByEmail(email: string): Promise<User> {
-        const user = await this.findOne({ where: { email } });
-        if (!user) throw new Error(rm.NO_USER);
-        return user;
+  static async findByEmail(email: string): Promise<User> {
+    const user = await this.findOne({ where: { email } });
+    if (!user) throw new Error(rm.NO_USER);
+    return user;
+  };
+
+
+  static async findOneByRefreshToken(token: string): Promise<User> {
+    const user = await this.findOne({ where: { refreshtoken: token } });
+    if (!user) throw new Error(rm.NO_USER);
+    return user;
+  };
+
+  
+  static async findById(id: number): Promise<User> {
+    const user = await this.findOne({ where: { id } });        
+    if (!user) throw new Error(rm.NO_USER);
+    return user;
+  }
+
+
+  static async findByIdAndResetNickname(id: number, nickname: string): Promise<User> {
+    await this.update({
+      nickname
+    }, {
+      where: { id }
+    });
+    const user =  await this.findOne({ where: { id } });
+    if (!user) throw new Error(rm.NO_USER);
+    return user;
+  };
+
+  static async createSocialUser(
+    social: string, 
+    userInfo: UserInfo, 
+    request: TokenDto, 
+    refreshtoken: Token
+  ): Promise<User> {
+    return await this.create({
+      social,
+      email: userInfo.email,
+      nickname: userInfo.nickname,
+      password: null,
+      fcmtoken: request.fcmtoken,
+      refreshtoken
+    });
+  };
+
+  static async findByEmailOrCreateSocialUser(
+    social: string,
+    userInfo: UserInfo,
+    request: TokenDto, 
+    refreshtoken: Token
+  ): Promise<User> { 
+    // 새 객체가 생성되었을 경우 true, 그렇지 않을 경우 false 
+    const user = await this.findOrCreate({
+      raw: true,
+      where: { email: userInfo.email },
+      defaults: {
+        social,
+        email: userInfo.email,
+        nickname: userInfo.nickname,
+        password: null,
+        // fcmtoken: request.fcmtoken,
+        refreshtoken
+      }
+    });
+
+    if (user[1] === true) { // 회원가입
+      user[0].nickname = null;
+      return user[0];
     }
 
-    static async findOneByRefreshToken(token: string): Promise<User> {
-        const user = await this.findOne({ where: { refreshtoken: token } });
-        if (!user) throw new Error(rm.NO_USER);
-        return user;
-    }
+    return user[0]; // boolean 값 빼고 반환
+  };
 
-    static async findById(id: number): Promise<User> {
-        const user = await this.findOne({ where: { id } });        
-        if (!user) throw new Error(rm.NO_USER);
-        return user;
-    }
+  // 기본적으로 delete는 유저 정보 반환 안하므로 custom 해줌 (삭제된 유저 정보 얻기 위해)
+  static async findAndDelete(id: number): Promise<User> {
+    return await this.findOne({ where: { id } })
+      .then(async resolve => {
+        await this.destroy({ where: { id } });
+        return resolve?.getDataValue("id");
+      });
+  };
 
 
-    static async findByIdAndResetNickname(id: number, nickname: string): Promise<User> {
-        await this.update({
-            nickname
-        }, {
-            where: { id }
-        });
-        const user =  await this.findOne({ where: { id } });
-        if (!user) throw new Error(rm.NO_USER);
-        return user;
-    }
+  static async setNickname(id: number, nickname: string): Promise<void> {
+    await this.update({
+      nickname,
+    }, {
+      where: { id }
+    });
+  };
 
-    static async createSocialUser(
-        social: string, 
-        userInfo: UserInfo, 
-        request: TokenDto, 
-        refreshtoken: Token
-    ): Promise<User> {
-        const user = await this.create({
-            social,
-            email: userInfo.email,
-            nickname: userInfo.nickname,
-            password: null,
-            fcmtoken: request.fcmtoken,
-            refreshtoken
-        });
-        return user;
-    }
 
-    static async findByEmailOrCreateSocialUser(
-        social: string,
-        userInfo: UserInfo,
-        request: TokenDto, 
-        refreshtoken: Token
-    ): Promise<User> { 
-        // 새 객체가 생성되었을 경우 true, 그렇지 않을 경우 false 
-        const user = await this.findOrCreate({
-            raw: true,
-            where: { email: userInfo.email },
-            defaults: {
-                social,
-                email: userInfo.email,
-                nickname: userInfo.nickname,
-                password: null,
-                // fcmtoken: request.fcmtoken,
-                refreshtoken
-            }
-        });
+  static async getFCMToken(id: number): Promise<String | undefined> {
+    const user = await this.findOne({
+      where: { id },
+      attributes: ["fcmtoken"]
+    });
+    return user?.fcmtoken;
+  };
 
-        if (user[1] === true) { // 회원가입
-            user[0].nickname = null;
-            return user[0];
-        }
 
-        return user[0]; // boolean 값 빼고 반환
-    }
-
-    // 기본적으로 delete는 유저 정보 반환 안하므로 custom 해줌 (삭제된 유저 정보 얻기 위해)
-    static async findAndDelete(id: number): Promise<User> {
-        return await this.findOne({ where: { id } })
-            .then(async resolve => {
-                await this.destroy({ where: { id } });
-                return resolve?.getDataValue("id");
-            });
-    }
-
-    static async setNickname(id: number, nickname: string): Promise<void> {
-        await this.update({
-            nickname,
-        }, {
-            where: { id }
-        });
-    }
-
-    static async getFCMToken(id: number): Promise<String|undefined> {
-        const user = await this.findOne({
-            where: {id},
-            attributes: ["fcmtoken"]
-        });
-        return user?.fcmtoken;
-    }
-
-    // static async getUserInfo(id: number): Promise<User> {
-    //     const user = await this.findOne({
-    //         where: { id },
-    //         include: [{
-    //             model: Time, attributes: ["morning", "afternoon", "night"]
-    //         }, {
-    //             model: UserCategory, attributes: ["category_id"]
-    //         }],
-    //         attributes: ["email", "nickname"]
-    //     });
-    //     if (!user) throw new Error(rm.NO_USER);
-    //     return user;
-    // }
+  static async getUserTimeReserveInfo(userIds: number[]): Promise<User[]> {
+    const usersInfo = await this.findAll({
+      where: { id: { [Op.in]: userIds } },
+      include: [
+        { model: Time, attributes: ["morning", "afternoon", "night"]}, 
+        { model: Reservation, attributes: ["id", "sendingDate"] }
+      ],
+      attributes: ["id"]
+    });
+    if (!usersInfo) throw new Error(rm.NO_USER);
+    return usersInfo;
+  };
 
 }

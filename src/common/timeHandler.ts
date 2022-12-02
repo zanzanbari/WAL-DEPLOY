b6812762fe0588b3f0bf3ad5036dc3518db0ae12
young <jinyoung7165@gmail.com>
2022-12-02 22:06:32 +0900

    [FIX] current Time

--- src/common/timeHandler.ts
+++ src/common/timeHandler.ts
@@ -17,7 +17,7 @@ const getCurrentTime = () => {
 }
 
 const getCurrentDate = () => {
-  return dayjs(new Date()).locale("ko").format("YYYY-MM-DD");
+  return toStringByFormatting(new Date());
 }
 
 
@@ -92,6 +92,22 @@ const toUtcTime = (date: Date) => {
     0
   );
   return utcTime;
+}
+
+function leftPad(value) {
+  if (value >= 10) {
+      return value;
+  }
+
+  return `0${value}`;
+}
+
+function toStringByFormatting(source, delimiter = '-') {
+  const year = source.getFullYear();
+  const month = leftPad(source.getMonth() + 1);
+  const day = leftPad(source.getDate());
+
+  return [year, month, day].join(delimiter);
 }
 
 const timeHandler = {
